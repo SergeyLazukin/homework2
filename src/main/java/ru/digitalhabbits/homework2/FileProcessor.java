852ca17a555f050489fe8ce319c2c06844b141cc
SergeyLazukin <Lazykun@mail.ru>
2020-12-18 14:45:26 +0300

    Refactoring

--- src/main/java/ru/digitalhabbits/homework2/FileProcessor.java
+++ src/main/java/ru/digitalhabbits/homework2/FileProcessor.java
@@ -69,7 +69,6 @@ public class FileProcessor {
         }
 
         // TODO: NotImplemented: остановить поток writerThread
-
         threadFileWriter.interrupt();
         executorService.shutdown();
 
